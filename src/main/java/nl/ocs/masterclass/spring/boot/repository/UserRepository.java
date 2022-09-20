8b41a29a74ebdec1d7e7a22846ac7b98260ebaaf
willemvd <willem@opencirclesolutions.nl>
2022-09-20 16:47:21 +0200

    use JpaRepository instead of CrudRepository to get more functions

--- src/main/java/nl/ocs/masterclass/spring/boot/repository/UserRepository.java
+++ src/main/java/nl/ocs/masterclass/spring/boot/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package nl.ocs.masterclass.spring.boot.repository;
 
 import nl.ocs.masterclass.spring.boot.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByName(String name);
 }
